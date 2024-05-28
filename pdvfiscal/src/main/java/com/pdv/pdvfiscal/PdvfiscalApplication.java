5a4bd82f30b66f9a366a13762a54e90c7a9f3137
Lucas Floriano da Silva <2022210220006@iesp.edu.br>
2024-05-28 01:06:21 -0300

    Update

---
+++ pdvfiscal/src/main/java/com/pdv/pdvfiscal/PdvfiscalApplication.java
@@ -0,0 +1,13 @@
+package com.pdv.pdvfiscal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdvfiscalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdvfiscalApplication.class, args);
+	}
+
+}
